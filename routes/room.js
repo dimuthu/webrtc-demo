5eb39f4d06639785c53c41b29024a3f8d7dcee17
Dimuthu Nilanka <dimuthu.nilanka@gmail.com>
2014-08-18 11:52:28 +0530

    remove some unnecessary files

--- routes/room.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/room/:roomId', function(req, res) {
-  res.render('room');
-});
-
-module.exports = router;
